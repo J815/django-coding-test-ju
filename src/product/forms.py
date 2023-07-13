def2c4bf6adefa1edd9fe889db34cf8f7b913df8
jubayer <jubayermd327@gmail.com>
2023-07-13 20:53:14 +0600

    second

--- src/product/forms.py
+++ src/product/forms.py
@@ -1,4 +1,5 @@
 from django.forms import forms, ModelForm, CharField, TextInput, Textarea, BooleanField, CheckboxInput
+from django import forms
 
 from product.models import Variant
 
@@ -12,3 +13,11 @@ class VariantForm(ModelForm):
             'description': Textarea(attrs={'class': 'form-control'}),
             'active': CheckboxInput(attrs={'class': 'form-check-input', 'id': 'active'})
         }
+
+
+class ProductFilterForm(forms.Form):
+    title = forms.CharField(required=False)
+    variant = forms.CharField(required=False)
+    price_from = forms.DecimalField(required=False, min_value=0)
+    price_to = forms.DecimalField(required=False, min_value=0)
+    date = forms.DateField(required=False)
